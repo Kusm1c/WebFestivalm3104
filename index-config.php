394ae8dd6647abc3c7d02463f27e574208520b4b
LucasConte <lucas.co.pro@gmail.com>
2021-12-05 15:49:26 +0100

    Ajout index-config.php

---
+++ index-config.php
@@ -0,0 +1,10 @@
+<?php
+//Fichier de configuration index.php
+
+//Include Flight
+require '../../includes/flight-master/flight/Flight.php';
+//Include Smarty libs
+require '../../includes/smarty/libs/Smarty.class.php';
+//Include PDO
+require '../../includes/pdo.php';
+?>
