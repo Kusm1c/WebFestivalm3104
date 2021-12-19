477057dc7dd91aaccbb025b82f433460387d17a1
LucasConte <lucas.co.pro@gmail.com>
2021-12-19 21:13:09 +0100

    Flight assign admin var - index.php

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
-<?php 
+<?php
 session_start();
 require 'index-config.php';
 
-Flight::set('db',$db);
+Flight::set('db', $db);
 
 // Register Smarty as the view class
 // Also pass a callback function to configure Smarty on load
-Flight::register('view', 'Smarty', array(), function($smarty){
+Flight::register('view', 'Smarty', array(), function ($smarty) {
     $smarty->template_dir = './templates/';
     $smarty->compile_dir = './templates_c/';
     $smarty->config_dir = './config/';
-    $smarty->cache_dir = './cache/';    
+    $smarty->cache_dir = './cache/';
 });
 
-Flight::map('render', function($template, $data){
+Flight::map('render', function ($template, $data) {
     Flight::view()->assign($data);
     Flight::view()->display($template);
 });
 
-if (isset($_SESSION["mail"]))
-{
-     Flight::view()->assign("__session__", $_SESSION["mail"]);
+if (isset($_SESSION["email"])) {
+    Flight::view()->assign("_session_", $_SESSION["email"]);
+    if ($_SESSION["isAdmin"] == 1)
+        Flight::view()->assign("_admin_", $_SESSION["isAdmin"]);
 }
 
 require "routes.php";
